495571d115bc7e3711d0aef4a38b1d976759c608
Nebay <ricardoyaben@hotmail.es>
2018-03-01 00:23:23 +0100

    W4P6 added

---
+++ src/W4P6.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+/**
+ * Week 4 – Problem 6
+ * Write a program that check if a string is a palindrome.
+ * NOTE: Use the method charAt(), not reverse().
+ */
+
+public class W4P6 {
+    public static void main(String[]arg){
+        Scanner scanner = new Scanner(System.in);
+        String string = scanner.nextLine().toLowerCase();
+        StringBuilder stringbuidler = new StringBuilder();
+
+        for(int x = 0; x < string.length(); x++) {
+            if (Character.isLetterOrDigit(string.charAt(x))) {
+                stringbuidler.append(string.charAt(x));
+            }
+        }
+        string = stringbuidler.toString();
+
+        int x = 0;
+        int i = string.length() -1;
+        while((x < string.length())&&( i>=0)){
+            if(string.charAt(x) == string.charAt(i)){
+                x++;
+                i--;
+            }else{
+                System.out.print("The sentence is NOT a palindrome.");
+                break;
+            }
+            if(i ==0){
+                System.out.print("The sentence is a palindrome.");
+            }
+        }
+
+    }
+}
