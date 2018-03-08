876001759650331dd096f609be9635a6622b0f7e
Nebay <ricardoyaben@hotmail.es>
2018-03-08 19:02:00 +0100

    Organized by weeks.

--- src/W1P2.java
+++
@@ -1,5 +0,0 @@
-public class W1P2 {
-    public static void main(String[]args){
-        System.out.printf("%s%n%s%n%s%n%s","    J     A   V     V   A","    J    A A   V   V   A A","J   J   AAAAA   V V   AAAAA"," J J   A     A   V   A     A");
-    }
-}
