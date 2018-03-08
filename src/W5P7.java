9c856c2fc04a8f6fdcda5839889823b9f9933da6
Nebay <ricardoyaben@hotmail.es>
2018-03-08 18:43:26 +0100

    W5P7 added

--- src/W5P7.java
+++ src/W5P7.java
@@ -1,40 +1,45 @@
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ *  Week 5 – Problem 7
+ *  Write a program that prompts the user to enter the number of students and each student’s name and score. The
+ *  program stops reading input when 0 is entered into the console. The program finally displays the student with the
+ *  highest score and the student with the second-highest score.
+ *  All grades are different.
+ */
 public class W5P7 {
 
     public static void main(String[]arg){
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("enter number of students and each student’s name and score.");
-        int a = scanner.nextInt();
+        ArrayList<Integer> array = new ArrayList<>();
+        ArrayList<String> strings = new ArrayList<>();
 
-        int[] array = new int[a];
-        String[] strings = new String[a];
+        for(int x=0; x!=-1; x++){
+            strings.add(x,scanner.next());
+            if(strings.get(x).equals("0")){
+                break;
+            }
+            array.add(x,scanner.nextInt());
 
-        for(int x = 0; x < a; x++){
-            strings[x] = scanner.next();
-            array[x] = scanner.nextInt();
-            if(array[x] == 0) {
-                try {
-                 x++;
-                 strings[x] = scanner.next();
-                 array[x] = scanner.nextInt();
-                }catch(Exception eta){
-                    break;
+            for(int i= 0; i<= x; i++){
+                if(array.get(i)<= array.get(x)){
+                    int temp = array.get(i);
+                    array.add(i,array.get(x));
+                    array.add(x,temp);
+
+                    String tempString = strings.get(i);
+                    strings.add(i,strings.get(x));
+                    strings.add(x,tempString);
                 }
             }
-            for(int i = 0; i < x;i++){
-                if(array[x] > array[i]){
-                    int o = array[x];
-                    array[x] = array[i];
-                    array[i] = o;
 
-                    String string = strings[x];
-                    strings[x] = strings[i];
-                    strings[i] = string;
-                }
-            }
         }
-        System.out.printf("1. %s %d%n2. %s %d",strings[0], array[0], strings[1], array[1]);
+        for(int j = 0; j<= 1; j++){
+            System.out.println((j+1)+". "+strings.get(j)+" "+array.get(j));
+        }
     }
 }
+
