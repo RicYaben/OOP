ffd5e3f41d4457a32cff0b88e06deb7284b177ce
Nebay <ricardoyaben@hotmail.es>
2018-03-08 17:19:09 +0100

    W5P2 added/fixed

--- src/W5P2.java
+++ src/W5P2.java
@@ -1,40 +1,54 @@
-import java.util.*;
+import java.util.Scanner;
+
+/**
+ *
+ *  Week 5 – Problem 2
+ *  A mail-order house sells five products whose retail prices are as follows:
+ *  • Product 1, $2.98;
+ *  • product 2, $4.50;
+ *  • product 3, $9.98;
+ *  • product 4, $4.49;
+ *  • and product 5, $6.87.
+ *  Write an application that reads a series of pairs of numbers as follows: a) productnumber
+ *  b) quantity sold.
+ *  Your program should use a switch statement to determine the retail price for each product. It should calculate and
+ *  display the total retail value of all products sold.
+ *  Use a sentinel-controlled loop to stop looping when reading 0 as product number and display the final results.
+ *  The double numbers should be printed with two digits after the decimal point. Use the template in the next page as
+ *  a starting point.
+ */
 
 public class W5P2 {
 
-    public static void main(String[]arg) {
+    public static void main(String[] arg) {
 
         Scanner scanner = new Scanner(System.in);
 
-        int a = 10;
-        int b = 20;
-        int c = 30;
-        int d = 40;
-        int e = 50;
-        int x;
-        int y;
-
-
-
-        switch (x = scanner.nextInt()){
-            case 1: x = a;
-            case 2: x = b;
-            case 3: x = c;
-            case 4: x = d;
-            case 5: x = e;
-            break;
+        double[] array = {0, 2.98, 4.50, 9.98, 4.49, 6.87};
+        int[] arrayInt = new int[6];
+        for(int x = scanner.nextInt(); x!= 0; x = scanner.nextInt()){
+            switch(x){
+                case 1:
+                    arrayInt[1] = arrayInt[1]+ scanner.nextInt();
+                    break;
+                case 2:
+                    arrayInt[2] = arrayInt[2]+ scanner.nextInt();
+                    break;
+                case 3:
+                    arrayInt[3] = arrayInt[3]+ scanner.nextInt();
+                    break;
+                case 4:
+                    arrayInt[4] = arrayInt[4]+ scanner.nextInt();
+                    break;
+                case 5:
+                    arrayInt[5] = arrayInt[5]+ scanner.nextInt();
+                    break;
+            }
         }
-        switch (y = scanner.nextInt()){
-            case 1: y = a;
-            case 2: y = b;
-            case 3: y = c;
-            case 4: y = d;
-            case 5: y = e;
-            break;
+        for(int i = 1; i<array.length; i++){
+            System.out.printf("Product %d: $%.2f%n",i, array[i]*arrayInt[i]);
         }
 
-        int mult = x * y;
-
-        System.out.println(mult);
     }
 }
+
